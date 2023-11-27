197780fc4dfb0ae383e927344f05e6a9b2a65ec0
kaisuhanda <kaisuhanda@gmail.com>
2023-11-27 16:04:16 +0700

    kurang mobile

--- src/components/HeadingDetails.jsx
+++ src/components/HeadingDetails.jsx
@@ -1,35 +1,44 @@
+import React from 'react'
 import './eventDetailsComponents.css'
 
-function headingDetails({ event, city }) {
-    return (
-        <div className="headingDetails">
-            <h2>{event?.name}</h2>
-            <ul>
-                <li>
-                    <div className="iContainer">
-                        <i className="fa-solid fa-calendar"></i>
-                    </div>
-                    {(event?.start_date)}
-                </li>
-                <li>
-                    <div className="iContainer">
-                        <i className="fa-solid fa-clock"></i>
-                    </div>
-                    {(event?.start_date)}
-                </li>
-                <li>
-                    <div className="iContainer">
-                        <i className="fa-solid fa-location-dot"></i>
-                    </div>
-                    {event?.location}, {city}
-                </li>
-            </ul>
-            <div className="seperator"></div>
-            <div className="eventPromoter">
-                made by <br /> {event?.promoter_id}
-            </div>
-        </div>
-    )
+function HeadingDetails({ event, city }) {
+  if (!event) {
+    return null
+  }
+
+  const start_date = event.start_date ? event.start_date : ''
+  const date = start_date.slice(0, 10)
+  const time = start_date.slice(11, 16)
+
+  return (
+    <div className="headingDetails">
+      <h2>{event.name}</h2>
+      <ul>
+        <li>
+          <div className="iContainer">
+            <i className="fa-solid fa-calendar"></i>
+          </div>
+          {date}
+        </li>
+        <li>
+          <div className="iContainer">
+            <i className="fa-solid fa-clock"></i>
+          </div>
+          {time}
+        </li>
+        <li>
+          <div className="iContainer">
+            <i className="fa-solid fa-location-dot"></i>
+          </div>
+          {event.location}, {city}
+        </li>
+      </ul>
+      <div className="seperator"></div>
+      <div className="eventPromoter">
+        Brought to you by Ticket Wave
+      </div>
+    </div>
+  )
 }
 
-export default headingDetails
+export default HeadingDetails
--- src/components/ticket.jsx
+++ src/components/ticket.jsx
@@ -1,31 +1,54 @@
-function Ticket() {
+import './eventDetailsComponents.css'
+
+function Ticket({ total, cart }) {
     return (
         <div className="ticket">
             <div className="checkoutContainer">
                 <div className="checkout">
-                    <div className="iContainerlarge">
-                        <i class="fa-solid fa-ticket-simple"></i>
-                    </div>
-                    <p>You haven't chosen a ticket. Please choose a ticket</p>
+                    {cart.length > 0 ? (
+                        <ul className='cartTickets'>
+                            {cart.map((cartTicket, index) => (
+                                <li key={index}>
+                                    <div className="flexboxTop">
+                                        <div className="iContainerlarge">
+                                            <i class="fa-solid fa-ticket-simple"></i>
+                                        </div>
+                                        <span>{cartTicket.type}</span>
+                                    </div>
+                                    <div className="flexbox">
+                                        <p>{cartTicket.quantity} tickets</p>
+                                        <span>Rp. {cartTicket.price * cartTicket.quantity}</span>
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <div className="div">
+                            <div className="iContainerlarge">
+                                <i class="fa-solid fa-ticket-simple"></i>
+                            </div>
+                            <p>You haven't chosen a ticket. Please choose a ticket</p>
+                        </div>
+                    )}
                 </div>
                 <div className="line2"></div>
                 <div className="total">
-                    <p>Price starts from</p>
-                    <h1>$$$$</h1>
+                    <p>Your total is</p>
+                    <span><h1>Rp. {Number(total)}</h1></span>
                 </div>
                 <div className="buttonContainer">
                     <button className="buyTicket">Buy Ticket</button>
                 </div>
             </div>
             <div className="share">
-                    Share this event
-                    <ul className="socialMediaIcons">
-                        <li><i class="fa-brands fa-twitter"></i></li>
-                        <li><i class="fa-brands fa-instagram"></i></li>
-                        <li><i class="fa-brands fa-whatsapp"></i></li>
-                        <li><i class="fa-solid fa-link"></i></li>
-                    </ul>
-                </div>
+                Share this event
+                <ul className="socialMediaIcons">
+                    <li><i class="fa-brands fa-twitter"></i></li>
+                    <li><i class="fa-brands fa-instagram"></i></li>
+                    <li><i class="fa-brands fa-whatsapp"></i></li>
+                    <li><i class="fa-solid fa-link"></i></li>
+                </ul>
+            </div>
         </div>
     )
 }
